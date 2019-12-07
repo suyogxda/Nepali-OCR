646a189d3f90c188a1ae36c55f3260564df29960
Suyog Adhikari <suyogxda@gmail.com>
2019-12-07 08:52:16 +0545

    Implement line extraction to main function

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,12 +1,27 @@
 #include <opencv2/opencv.hpp>
 #include "thin.h"
+#include "extractLines.h"
 
 int main(int argc, char** argv){
-  cv::Mat input_image = cv::imread(argv[1], 0);
-  cv::Mat thinned_image;
-  thinning(input_image, thinned_image);
+  cv::Mat input_image = cv::imread( argv[1], 0 );
+  cv::Mat thinned_image, binary_image;
+  
+  // We use inverse binary because what we want to pass...
+  // white characters on black background to the thinning function
+  cv::threshold( input_image, binary_image, 0, 255, cv::THRESH_BINARY_INV + cv::THRESH_OTSU );
 
-  cv::namedWindow("Thinned", cv::WINDOW_NORMAL);
-  cv::imshow("Thinned", thinned_image);
-  cv::waitKey(0);
+  // This function makes sure that the characters are reduced to pixels
+  thinning( binary_image, thinned_image );
+
+  // The line below returns vector of lines extracted
+  auto vector_of_images = segment_lines( thinned_image, binary_image );
+
+  // For loop to show the seperated lines.
+  // This line extraction method is not limited just to devnagari scripts
+  // Just make sure that the images of texts you pass are clean, and not connected vertically
+  cv::namedWindow( "Lines", cv::WINDOW_NORMAL );
+  for( size_t i = 0; i < vector_of_images.size(); ++i ){
+    cv::imshow( "Lines", vector_of_images[i] );
+    cv::waitKey();
+  }
 }
